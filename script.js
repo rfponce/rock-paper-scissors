421df7e39ded8c935a1cb4aed216e0786907113b
Fabián Ponce <maximohumano@gmail.com>
2022-09-09 15:36:19 -0500

    Fixed js code with an unreachable instruction

--- script.js
+++ script.js
@@ -11,15 +11,12 @@ function computerPlay() {
   switch (randomNumber) {
     case 1:
       return "rock";
-      break;
 
     case 2:
       return "paper";
-      break;
 
     case 3:
       return "scissors";
-      break;
   }
 }
 
